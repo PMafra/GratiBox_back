0c364c172af8ddc41a099103b4590910821b9750
PMafra <pmafrarm@gmail.com>
2021-11-20 22:04:41 -0300

    Comment - Insert plan in database

--- src/database/insert.sql
+++ src/database/insert.sql
@@ -1,6 +1,6 @@
-INSERT INTO products (name) VALUES ('Chás');
-INSERT INTO products (name) VALUES ('Insensos');
-INSERT INTO products (name) VALUES ('Produtos orgânicos');
+INSERT INTO products (name) VALUES ('Teas');
+INSERT INTO products (name) VALUES ('Insence');
+INSERT INTO products (name) VALUES ('Organic products');
 INSERT INTO "plans_types" (type) VALUES ('Weekly');
 INSERT INTO "plans_types" (type) VALUES ('Monthly');
 INSERT INTO plans (plan_type_id, day) VALUES (1, 'Monday');
@@ -9,3 +9,8 @@ INSERT INTO plans (plan_type_id, day) VALUES (1, 'Friday');
 INSERT INTO plans (plan_type_id, day) VALUES (2, '1');
 INSERT INTO plans (plan_type_id, day) VALUES (2, '10');
 INSERT INTO plans (plan_type_id, day) VALUES (2, '20');
+
+-- INSERT INTO users_plans (user_id, plan_id, signature_date) VALUES (6, 1, NOW());
+-- INSERT INTO users_plans_products (user_plan_id, product_id) VALUES (3, 1);
+-- INSERT INTO users_plans_products (user_plan_id, product_id) VALUES (3, 2);
+-- INSERT INTO users_plans_products (user_plan_id, product_id) VALUES (3, 3);
