173d537b302fa635284469f49a31b3214455a2e4
Matheus Galdino <matheus@respondeai.com.br>
2021-11-23 12:20:35 -0300

    preparando o review

--- src/shared/variables.js
+++
@@ -1,8 +0,0 @@
-const selectUsers = 'SELECT * FROM "users"';
-const passwordRules = 'Your password must contain at least 8 characters, 1 upper case letter, 1 lower case letter, 1 number and 1 special character.';
-const CPFRules = 'Your cpf must only contain 11 numbers';
-const incorrectInputMessage = 'Email or password is invalid!';
-
-export {
-  selectUsers, passwordRules, CPFRules, incorrectInputMessage,
-};
